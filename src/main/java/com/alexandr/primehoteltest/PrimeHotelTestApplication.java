a29abb84d12df91e703a5104fbfae784fcbea166
Alexandr Glukhov <alexandrgluhov@mail.ru>
2024-04-26 01:26:34 +0300

    Init commit

---
+++ src/main/java/com/alexandr/primehoteltest/PrimeHotelTestApplication.java
@@ -0,0 +1,13 @@
+package com.alexandr.primehoteltest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeHotelTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeHotelTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alexandr/primehoteltest/PrimeHotelTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexandr.primehoteltest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeHotelTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
